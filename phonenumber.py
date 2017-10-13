59525d347acb87c0e67b07ea9da80935459275b7
jianhuayan <jianhuayan@users.noreply.github.com>
2017-10-13 11:53:50 -0400

    adding a file to test commit and push

---
+++ phonenumber.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.7
+import sys
+import math
+
+###################################################################################
+# This is programe to find a specific phone number from a list of files           #
+#                                                                                 #
+#              Created by Jianhua Yan for pindrop evaluation                      #
+#                                                                                 #
+###################################################################################
+
+filelist = [a.txt, b.txt, c.txt]
+phonenumber
+
+# find wether the phone number
+def phone
